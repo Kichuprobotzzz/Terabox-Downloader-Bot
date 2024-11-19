b683e04e3ca40fd0b7c452536229170d40f56de1
Kichuprobotzzz <111451450+Kichuprobotzzz@users.noreply.github.com>
2024-11-19 19:17:11 +0530

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -54,7 +54,7 @@ async def start_command(client, message):
     user_mention = message.from_user.mention
     reply_message = f"<b>ᴡᴇʟᴄᴏᴍᴇ, {user_mention}.\n\n🌟 ɪ ᴀᴍ ᴀ ᴛᴇʀᴀʙᴏx ᴅᴏᴡɴʟᴏᴀᴅᴇʀ ʙᴏᴛ. sᴇɴᴅ ᴍᴇ ᴀɴʏ ᴛᴇʀᴀʙᴏx ʟɪɴᴋ ɪ ᴡɪʟʟ ᴅᴏᴡɴʟᴏᴀᴅ ᴡɪᴛʜɪɴ ғᴇᴡ sᴇᴄᴏɴᴅs ᴀɴᴅ sᴇɴᴅ ɪᴛ ᴛᴏ ʏᴏᴜ ✨.\n\n⮕ ꜱᴘᴇᴇᴅ: 99.99ᴍʙ/ꜱ \n⮕ ᴘʟᴀɴ: ᴜɴʟɪᴍɪᴛᴇᴅ</b>"
     join_button = InlineKeyboardButton("ᴊᴏɪɴ ", url="https://t.me/Dramaship")
-    developer_button = InlineKeyboardButton("ᴀʟʟ ʙᴏᴛꜱ", url="https://t.me/Teraboxdev/12")
+    developer_button = InlineKeyboardButton("ᴀʟʟ ʙᴏᴛꜱ", url="https://t.me/AMBotList/12")
     reply_markup = InlineKeyboardMarkup([[join_button, developer_button]])
     video_file_id = "/app/Jet-Mirror.mp4"
     if os.path.exists(video_file_id):
